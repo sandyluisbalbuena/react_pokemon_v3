f31cb9727ed5e0b7b355d1a69f9152cdeebffb52
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-06-20 08:36:58 +0800

    wew

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import eventBus from '../eventBus';
-import  { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import firebase from 'firebase/compat/app';
 import { useNavigate } from 'react-router-dom';
 
-
 const Header = () => {
-
   const [user] = useAuthState(firebase.auth());
-	const navigate = useNavigate();
+  const navigate = useNavigate();
   let [activeLink, setActiveLink] = useState('');
   const location = useLocation();
   const [username, setUsername] = useState('');
@@ -45,185 +43,265 @@ const Header = () => {
       });
     }
   }, [user]);
-  // console.log(user);
 
   const handleSearch = () => {
     eventBus.publish('searchPokemon', document.getElementById('pokemonNameInputSearch').value);
-  }
+  };
 
   const handleCardSearch = () => {
     eventBus.publish('getonecard', document.getElementById('pokemonCardNameInputSearch').value);
-  }
+  };
 
   const handleKeyPress = (event) => {
-
-    if(document.getElementById('pokemonNameInputSearch')){
+    if (document.getElementById('pokemonNameInputSearch')) {
       if (event.key === 'Enter') {
         handleSearch();
       }
-    }
-    else {
+    } else {
       if (event.key === 'Enter') {
         handleCardSearch();
       }
     }
   };
 
-  async function handleLogout (){
-		try {
-			await firebase.auth().signOut();
+  async function handleLogout() {
+    try {
+      await firebase.auth().signOut();
 
       Swal.fire({
-				icon: 'success',
-				text: 'Successfully Logout.',
-				footer: '<a href="">Why do I have this issue?</a>'
-			})
+        icon: 'success',
+        text: 'Successfully Logout.',
+        footer: '<a href="">Why do I have this issue?</a>',
+      });
 
       const userRef = firebase.database().ref(`onlineUsers`);
-      userRef.child(user.uid).set(false); 
+      userRef.child(user.uid).set(false);
       // userRef.update(false);
 
-    navigate('/');
-		} catch (error) {
+      navigate('/');
+    } catch (error) {
       Swal.fire({
-				icon: 'error',
-				title: 'Oops...',
-				text: error,
-				footer: '<a href="">Why do I have this issue?</a>'
-			})
-		}
-	};
+        icon: 'error',
+        title: 'Oops...',
+        text: error,
+        footer: '<a href="">Why do I have this issue?</a>',
+      });
+    }
+  }
 
+  const closeMobileNavbar = () => {
+    const navbarToggle = document.getElementById('navbarSupportedContent');
+    if (navbarToggle.classList.contains('show')) {
+      navbarToggle.classList.remove('show');
+    }
+  };
 
   return (
-	<header>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <a className="navbar-brand mt-2 mt-lg-0" href="/">
-            <img
-              src="../assets/images/brand/pokemonBrandName.png"
-              height="30"
-              alt="Pokemon"
-              loading="lazy"
-            />
-          </a>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <a className="navbar-brand mt-2 mt-lg-0" href="/">
+              <img
+                src="../assets/images/brand/pokemonBrandName.png"
+                height="30"
+                alt="Pokemon"
+                loading="lazy"
+              />
+            </a>
 
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to='/' onClick={() => handleNavLinkClick('home')} className="nav-link hvr-underline-from-center {activeLink === 'pokedex' ? 'active' : ''}">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/pokedex' onClick={() => handleNavLinkClick('pokedex')} className="nav-link hvr-underline-from-center {activeLink === 'pokedex' ? 'active' : ''}">Pokedex</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/pokecard' onClick={() => handleNavLinkClick('pokecard')} className="nav-link hvr-underline-from-center {activeLink === 'pokedex' ? 'active' : ''}">Pokecard</Link>
-            </li>
-            {user ? (
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  onClick={() => {
+                    handleNavLinkClick('home');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'home' ? 'active' : ''
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/pokedex"
+                  onClick={() => {
+                    handleNavLinkClick('pokedex');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'pokedex' ? 'active' : ''
+                  }`}
+                >
+                  Pokedex
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/pokecard"
+                  onClick={() => {
+                    handleNavLinkClick('pokecard');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'pokecard' ? 'active' : ''
+                  }`}
+                >
+                  Pokecard
+                </Link>
+              </li>
+              {user ? (
                 <Link
                   to="/pokeforum"
-                  onClick={() => handleNavLinkClick('pokeforum')}
-                  className={`nav-link hvr-underline-from-center ${activeLink === 'pokeforum' ? 'active' : ''}`}
+                  onClick={() => {
+                    handleNavLinkClick('pokeforum');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'pokeforum' ? 'active' : ''
+                  }`}
                 >
                   Pokeforum
                 </Link>
               ) : (
                 <Link
                   to="/login"
-                  className={`nav-link hvr-underline-from-center ${activeLink === 'login' ? 'active' : ''}`}
+                  onClick={() => {
+                    handleNavLinkClick('login');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'login' ? 'active' : ''
+                  }`}
                 >
                   Pokeforum
                 </Link>
               )}
               <li className="nav-item">
-                <Link to='/about' onClick={() => handleNavLinkClick('about')} className="nav-link hvr-underline-from-center {activeLink === 'pokedex' ? 'active' : ''}">About</Link>
-            </li>
-          </ul>
-
-          {activeLink === 'pokedex' && (
-            <div id="pokemonSearchBar" className="d-flex input-group w-auto me-5">
-              <input id="pokemonNameInputSearch" type="search" className="form-control rounded" placeholder="Pokemon Name" aria-label="Search" aria-describedby="search-addon" onKeyPress={handleKeyPress} required/>
-              <button className="btn bg-dark" onClick={handleSearch}>
-                <i className="fas fa-search text-white"></i>
-              </button>
-            </div>
-          )}
-
-          {activeLink === 'pokecard' && (
-            <div id="pokemonCardSearchBar" className="d-flex input-group w-auto me-5">
-              <input id="pokemonCardNameInputSearch" type="search" className="form-control rounded" placeholder="Pokemon Card Name" aria-label="Search" aria-describedby="search-addon" onKeyPress={handleKeyPress} required/>
-              <button className="btn bg-dark" onClick={handleCardSearch}>
-                <i className="fas fa-search text-white"></i>
-              </button>
-            </div>
-          )}
-
-          {user ? (
-            <>
-              <div className="dropdown">
-                <a
-                  className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                  href="#"
-                  id="navbarDropdownMenuAvatar"
-                  role="button"
-                  data-mdb-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <span className={`nav-link text-white  ${activeLink === 'login' ? 'active' : ''}`}>
-                  {username !== '' ? username : user.displayName}
-                  </span>
-                  <div className="rounded-circle bg-secondary ms-3"><img className="m-1" width="30px" src={`../assets/images/userIcons/${userimage}.png`}/></div>
-                </a>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                  <li>
-                    <a className="dropdown-item"  data-mdb-toggle="modal" data-mdb-target="#profile" href="#">My profile</a>
-                  </li>
-                  {/* <li>
-                    <a className="dropdown-item" href="#">Settings</a>
-                  </li> */}
-                  <li>
-                    <a className="dropdown-item" onClick={handleLogout} href="#">Logout</a>
-                  </li>
-                </ul>
-              </div>
-            </>
-          ) : (
-            <ul className="navbar-nav mb-2 mb-lg-0  input-group w-auto me-5">
-              <li className="nav-item">
                 <Link
-                  to="/login"
-                  className={`nav-link hvr-underline-from-center ${activeLink === 'login' ? 'active' : ''}`}
+                  to="/about"
+                  onClick={() => {
+                    handleNavLinkClick('about');
+                    closeMobileNavbar();
+                  }}
+                  className={`nav-link hvr-underline-from-center ${
+                    activeLink === 'about' ? 'active' : ''
+                  }`}
                 >
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/register"
-                  className={`nav-link hvr-underline-from-center ${activeLink === 'register' ? 'active' : ''}`}
-                >
-                  Register
+                  About
                 </Link>
               </li>
             </ul>
-          )}
 
+            {activeLink === 'pokedex' && (
+              <div id="pokemonSearchBar" className="d-flex input-group w-auto me-5">
+                <input
+                  id="pokemonNameInputSearch"
+                  type="search"
+                  className="form-control rounded"
+                  placeholder="Pokemon Name"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                  onKeyPress={handleKeyPress}
+                  required
+                />
+                <button className="btn bg-dark" onClick={handleSearch}>
+                  <i className="fas fa-search text-white"></i>
+                </button>
+              </div>
+            )}
+
+            {activeLink === 'pokecard' && (
+              <div id="pokemonCardSearchBar" className="d-flex input-group w-auto me-5">
+                <input
+                  id="pokemonCardNameInputSearch"
+                  type="search"
+                  className="form-control rounded"
+                  placeholder="Pokemon Card Name"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                  onKeyPress={handleKeyPress}
+                  required
+                />
+                <button className="btn bg-dark" onClick={handleCardSearch}>
+                  <i className="fas fa-search text-white"></i>
+                </button>
+              </div>
+            )}
+
+            {!user ? (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    to="/register"
+                    onClick={() => {
+                      handleNavLinkClick('register');
+                      closeMobileNavbar();
+                    }}
+                    className={`nav-link hvr-underline-from-center ${
+                      activeLink === 'register' ? 'active' : ''
+                    }`}
+                  >
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/login"
+                    onClick={() => {
+                      handleNavLinkClick('login');
+                      closeMobileNavbar();
+                    }}
+                    className={`nav-link hvr-underline-from-center ${
+                      activeLink === 'login' ? 'active' : ''
+                    }`}
+                  >
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            ) : (
+              <div className="d-flex align-items-center">
+                <span className="navbar-text mx-2">{username}</span>
+                <Link
+                  to={`/profile/${user.uid}`}
+                  onClick={() => {
+                    handleNavLinkClick('profile');
+                    closeMobileNavbar();
+                  }}
+                >
+                  <img
+                    src={userimage ? userimage : '../assets/images/profile/pikachu.png'}
+                    alt="Profile"
+                    className="rounded-circle"
+                    width="30"
+                    height="30"
+                  />
+                </Link>
+                <button className="btn btn-link text-white" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
-  )
-}
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
